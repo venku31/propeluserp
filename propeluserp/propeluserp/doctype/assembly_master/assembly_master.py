df402b7376b66943b653aefc95bd97009eb2924b
venku31 <venku31@gmail.com>
2026-06-06 22:44:25 +0530

    Initial commit

---
+++ propeluserp/propeluserp/doctype/assembly_master/assembly_master.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Propelus and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class AssemblyMaster(Document):
+	pass
